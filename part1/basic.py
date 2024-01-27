690b2fe11ba96d659476ce7166566774dd441d84
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-01-27 19:23:26 +0530

    part1

---
+++ part1/basic.py
@@ -0,0 +1,115 @@
+# print("Welcome dinesh")
+# python is a case sensetive language
+# welcoem Message single line comment
+''' 
+hi 
+i  
+Am 
+dinesh'''  # miltiline unoffisial comment
+#  variable
+# in case of boolean variable value start with capital letter (True or False)
+# popup = True
+# name_ad = "dkp"
+# age = 10
+# height = 2.5
+
+# String
+
+# name = "dinesh kumar pal"
+
+# print(len(name))  # return length of variable ot-16
+# print(name[3])  # return third index value ot-e
+# print(name[-2])  # return second value from right side ot-a
+# spliceing (cuting in string)
+# print(name[0:5])  # start from 2 index and stop to 5 string count ot - dines
+# print(name[0:])  # start from 2 index and not  stop ot- dinesh kumar pal
+# start from 0 index and not  stop 5 count ot- dines
+# print(name[:5])
+
+# operators
+
+# a = 10
+# b = 10
+# c = "dkp"
+# print(a+b)
+# print(5**2)  # 5*5
+# print(5**4)  # 5*5*5*5
+# floor division (return division value in interger ) it remove  decimal digits
+# print(11//4)
+
+
+# logical operator
+# and or not (in javascript && || ! )
+# a = 10
+# b = 20
+# print(a >= 10 and b != 20)
+# print(a >= 10 or b != 20)
+# print(not b != 20)
+
+# membership operators  ##(in javascript arr.include(50))
+
+# name = "Dinesh"
+
+# print('d' in name)  # False because its a case sensetive language
+# print('e' in name)  # True
+# arr = [25, 5266, 636, 52]
+# print(50 not in arr)  # True    ##(in javascript arr.include(50))
+
+# identity operator
+# is (==) is not (!=)
+
+# a = 10
+# b = 10
+# print(a is b, b == a) # True True
+# print(a is not b, b != a) # False False
+
+# Bitwise operator
+
+# & if both side true then return True (and)
+# | if atleast one true then return True (or)
+# ^ if both side same then return Flase else True (xor)
+
+# x = 10
+# y = 8
+
+# print(bin(x))  # return binary value 1010
+# print(bin(y))  # return binary value 1000
+
+# print(bin(x & y), x & y)  # return 1000 (means 8)
+# print(bin(x | y), x | y)  # return 1010 (means 10)
+# print(bin(x ^ y), x ^ y)  # return 0010 (means 2)
+
+# DataType
+# a = 10  # number
+# b=2.5 float
+# c=4+5j complex
+
+# print(type(a)) # it return the datatype of a variable
+
+# d = '''
+# hello Dinesh
+# how are you today
+# i can  ...
+# '''
+# print(d, type(d))  # it work as a pre tag it means preformated text
+
+# list (array)
+
+# l = [25, "dkp", "ak"]
+# l[2] = "si"
+# print(l, type(l)) #  [25, 'dkp', 'si'] <class 'list'>
+
+# tuple (can not be change and atleast two value in bracket )
+
+# t = (2, 525, "dkp", "si")
+# print(t, type(t))  # (2, 525, 'dkp', 'si') <class 'tuple'>
+
+# Dictionary data type (it accept value in key and pair and we can acces useing its key and we can also update it)
+
+# d = {
+#     "name": "dinesh",
+#     "sName": "pal"
+# }
+# print(d)  # "{'name': 'dinesh', 'sName': 'pal'}
+# d["name"] = "Ganesh"
+# print(d, type(d))  # {'name': 'Ganesh', 'sName': 'pal'} <class 'dict'>
